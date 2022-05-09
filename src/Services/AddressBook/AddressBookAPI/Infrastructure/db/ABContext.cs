e0e58463b2c1d45afb840bf100fece5140162f12
ImadUdDin <emaduddin181@gmail.com>
2022-05-09 17:21:16 +0500

    Adding Database context, connection string and injecting it

---
+++ src/Services/AddressBook/AddressBookAPI/Infrastructure/db/ABContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AddressBook.Infrastructure.db
+{
+    public class ABContext : DbContext
+    {
+        public ABContext(DbContextOptions<ABContext> options) : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>().ToTable("ContactsAddressBook");
+        }
+    }
+    public class Contact
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public AddressType AddressType { get; set; }
+
+        public Contact()
+        {
+            AddressType = AddressType.Home;
+        }
+    }
+    public enum AddressType
+    {
+        Home = 0,
+        Work = 1,
+    }
+}
